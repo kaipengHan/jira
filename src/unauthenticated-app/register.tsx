4de20885cfbd103a6769be7245d58e7d3d1f9daa
hkp <sxhkp163@163.com>
2021-10-21 18:50:52 +0800

    style: 安装emotion 修改登录页面样式

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth } from "../context/auth-context";
-import { Button, Form, Input } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "./index";
 
 const RegisterScreen = () => {
   const { register } = useAuth();
@@ -26,9 +27,9 @@ const RegisterScreen = () => {
         <Input type="password" placeholder={"请输入密码"} />
       </Form.Item>
       <Form.Item>
-        <Button htmlType={"submit"} type={"primary"}>
+        <LongButton htmlType={"submit"} type={"primary"}>
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
